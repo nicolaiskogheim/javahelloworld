d8da8d1086e0ab0d0a33bc31e0b350f14c2a6686
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2015-12-25 23:11:23 +0100

    Initial commit

---
+++ JavaHelloWorld.java
@@ -0,0 +1,6 @@
+class JavaHelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World from a java-file running in docker!");
+        System.out.println("Hello again!");
+    }
+}
