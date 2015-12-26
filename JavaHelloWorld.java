0c5bfe8ca6de010654804ac15daf4a38cc607a08
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2015-12-26 02:32:11 +0100

    An edit

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,5 +2,6 @@ class JavaHelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello World from a java-file running in docker!");
         System.out.println("Hello again!");
+        System.out.println("An edit");
     }
 }
